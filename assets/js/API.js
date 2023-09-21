2bc100535a9854a4f010c834886d0570adc0628d
Isabella <bdc190799@gmail.com>
2023-09-21 20:31:28 +0100

    Added random movie generation
    Added random movie generation to the OMDB Api call. This random movie generation attempts to filter out adult content by removing R rated and Adult genre content. Also the random movie must contain a valid poster URL and rating value for use in website display and game functionality.

--- assets/js/API.js
+++ assets/js/API.js
@@ -11,71 +11,107 @@ let omdbMovieSearch = "The Matrix";
 let omdbAPIKey = "3646905f";
 let testOmdbAPIUrl = `http://www.omdbapi.com/?apikey=${omdbAPIKey}&t=${omdbMovieSearch}`;
 
-function populateFeedback(gif)
-{
+function populateFeedback(gif) {
     let giphyAPIUrl = `https://api.giphy.com/v1/gifs/search?api_key=${giphyAPIKey}&q=${gif}&limit=${giphySearchNumber}&offset=0&rating=pg-13&lang=en&bundle=messaging_non_clips`;
     fetch(giphyAPIUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function(content) //giphy response
-    {
-        console.log(content.data);
-        //referencing new image to create
-        let thisImage = $("<img id='temp'>");
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (content) //giphy response
+        {
+            console.log(content.data);
+            //referencing new image to create
+            let thisImage = $("<img id='temp'>");
 
-        //setting image attributes
-        thisImage.attr('src', content.data[0].images.fixed_height.url);
-        thisImage.attr('alt', content.data[0].title);
+            //setting image attributes
+            thisImage.attr('src', content.data[0].images.fixed_height.url);
+            thisImage.attr('alt', content.data[0].title);
 
-        //placing the new image on the site
-        $("#feedbackGif").append(thisImage); //
-        
-    })
-    .catch(error =>
-    {
-        console.error("Error in giphyAPI");
-        console.log(error);
-    });
+            //placing the new image on the site
+            $("#feedbackGif").append(thisImage); //
+
+        })
+        .catch(error => {
+            console.error("Error in giphyAPI");
+            console.log(error);
+        });
 }
 
+function generateRandomFilm() {
+    let randomId = Math.floor(Math.random() * 1000000) + 1;
+    if (randomId.toString().length)
+    //prepending 0's to the id to ensure the id fits imdb/omdb formatting styles.
+        switch (randomId.toString().length) {
+            case 1:
+                randomId = "000000" + randomId;
+                break;
+            case 2:
+                randomId = "00000" + randomId;
+                break;
+            case 3:
+                randomId = "0000" + randomId;
+                break;
+            case 4:
+                randomId = "000" + randomId;
+                break;
+            case 5:
+                randomId = "00" + randomId;
+                break;
+            case 6:
+                randomId = "0" + randomId;
+                break;
+            default:
+                console.error("Incorrect random length");
+                break;
+        }
+    console.log("tt" + randomId);
+    return "tt" + randomId;
+}
 //film = search query, filmNumber = if it will populate filmOne(1) or filmTwo(2)
-function populateFilms(film, filmNumber)
-{
-    let omdbAPIURL = `http://www.omdbapi.com/?apikey=${omdbAPIKey}&t=${film}`;
-    console.log(omdbAPIURL);
+function populateFilms(filmNumber) {
+    let searchTerm = generateRandomFilm();
+    let omdbAPIURL = `http://www.omdbapi.com/?apikey=${omdbAPIKey}&i=${searchTerm}`;
+    console.log("OMDB API URL = " + omdbAPIURL);
     fetch(omdbAPIURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(content) //omdb response
-    {
-        console.log(content);
-        //referencing new image to create
-        let thisImage = $("<img id='temp'>");
-
-        //setting image attributes
-        thisImage.attr('src', content.Poster);
-        thisImage.attr('alt', content.Title);
-
-        //placing the new image on the site
-        if(filmNumber === 1) //place poster on the left
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (content) //omdb response
         {
-            $("#filmOne").prepend(thisImage);
-        }
-        else if (filmNumber === 2) //place poster on the right
-        {
-            $("#filmTwo").prepend(thisImage);
-        }
-    })
-    .catch(error =>
-    {
-        console.error("Error in omdbAPI");
-    });
+            console.log(content);
+            //check if the id is valid and the content has a rating and the content has a poster to display. Also (tries to) filter NSFW content
+            if(content.response = true && content.imdbRating != "N/A" && content.Poster != "N/A" && content.Genre != "Adult" && content.Rated != "R")
+            {
+                //referencing new image to create
+                let thisImage = $("<img id='temp'>");
+
+                //setting image attributes
+                thisImage.attr('src', content.Poster);
+                thisImage.attr('alt', content.Title);
+
+                //placing the new image on the site
+                if (filmNumber === 1) //place poster on the left
+                {
+                    $("#filmOne").prepend(thisImage);
+                }
+                else if (filmNumber === 2) //place poster on the right
+                {
+                    $("#filmTwo").prepend(thisImage);
+                }
+            }
+            else
+            {
+                populateFilms(filmNumber); //regenerate the id and try again.
+                console.log("Regenerating film");
+            }
+        })
+        .catch(error => {
+            console.error("Error in omdbAPI");
+        });
 }
 
-
+generateRandomFilm();
 populateFeedback("Congratulations");
 //populateFeedback("The Avengers");
-populateFilms("The Matrix", 1);
-populateFilms("The Avengers", 2);
+populateFilms(1);
+populateFilms(2);
